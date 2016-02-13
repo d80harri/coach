15a992f5d92e7e3bc657605db593863a5bf16f20
d80harri <d80harri@d80harri-Lenovo-H520>
2016-02-13 16:44:45 +0100

    Introduced and tested AbstractDao

--- console-app/src/main/java/net/d80harri/coach/domain/console/Application.java
+++ console-app/src/main/java/net/d80harri/coach/domain/console/Application.java
@@ -1,19 +1,23 @@
 package net.d80harri.coach.domain.console;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.UUID;
 
 import net.d80harri.coach.console.ConsoleEngine;
+import net.d80harri.coach.domain.Entity;
 import net.d80harri.coach.domain.program.ProgramDao;
 import net.d80harri.coach.domain.program.ProgramEntity;
 
 public class Application {
 
 	public static void main(String[] args) throws IOException {
-		ProgramDao dataSource = new ProgramDao();
-		dataSource.add(new ProgramEntity(UUID.randomUUID(), "Plan 1"));
-		dataSource.add(new ProgramEntity(UUID.randomUUID(), "Plan 2"));
-		dataSource.add(new ProgramEntity(UUID.randomUUID(), "Plan 3"));
+		Collection<Entity> ds = new ArrayList<>();
+		ProgramDao dataSource = new ProgramDao(ds);
+		dataSource.put(new ProgramEntity(UUID.randomUUID(), "Plan 1"));
+		dataSource.put(new ProgramEntity(UUID.randomUUID(), "Plan 2"));
+		dataSource.put(new ProgramEntity(UUID.randomUUID(), "Plan 3"));
 
 		ConsoleEngine engine = new ConsoleEngine(new MainConsole(dataSource), System.in, System.out);
 		engine.start();
--- domain/src/main/java/net/d80harri/coach/domain/program/ProgramEntity.java
+++ domain/src/main/java/net/d80harri/coach/domain/program/ProgramEntity.java
@@ -7,9 +7,6 @@ import net.d80harri.coach.domain.Entity;
 public class ProgramEntity extends Entity {
 	private String name;
 	
-	public ProgramEntity() {
-	}
-
 	public ProgramEntity(UUID uuid, String name) {
 		super(uuid);
 		this.name = name;
