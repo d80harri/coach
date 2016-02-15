85658de3c5bcdbb420a2c74d226229de0b12a3ec
Harald Bittermann <harald.bittermann@gmail.com>
2016-02-15 14:02:12 +0100

    Configures jacoco code coverage plugin

--- domain/src/test/java/net/d80harri/coach/domain/DtoAssertions.java
+++ domain/src/test/java/net/d80harri/coach/domain/DtoAssertions.java
@@ -1,9 +1,11 @@
 package net.d80harri.coach.domain;
 
+import org.assertj.core.api.Assertions;
+
 import net.d80harri.coach.domain.program.Program;
 import net.d80harri.coach.domain.program.entities.Action;
 
-public class DtoAssertions {
+public class DtoAssertions extends Assertions {
 
 	public static ProgramAssert assertThat(Program dto) {
 		return new ProgramAssert(dto);
--- domain/src/test/java/net/d80harri/coach/domain/program/ProgramLogicTest.java
+++ domain/src/test/java/net/d80harri/coach/domain/program/ProgramLogicTest.java
@@ -3,6 +3,7 @@ package net.d80harri.coach.domain.program;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 import org.assertj.core.api.Assertions;
@@ -18,7 +19,7 @@ import net.d80harri.coach.domain.program.entities.SupersetActionEntity;
 public class ProgramLogicTest {
 
 	@Test
-	public void testRetrieveProgram() {
+	public void testGetByUuid() {
 		Collection<Entity> ds = new ArrayList<>();
 		ProgramDao dao = new ProgramDao(ds);
 		ProgramLogic logic = new ProgramLogic(dao, new DomainMapper());
@@ -36,6 +37,32 @@ public class ProgramLogicTest {
 		
 		Program readProgram = logic.getById(program.getUuid()).get();
 		
+		DtoAssertions.assertThat(readProgram).isNotNull().hasId(program.getUuid()).hasActionId(programAction.getUuid());
+		DtoAssertions.assertThat(readProgram.getAction()).hasId(programAction.getUuid()).isSequenceAction().containsExactlyActionIds(warmupAction.getUuid(), skill.getUuid(), strength.getUuid());
+	}
+	
+	@Test
+	public void testGetAll() {
+		Collection<Entity> ds = new ArrayList<>();
+		ProgramDao dao = new ProgramDao(ds);
+		ProgramLogic logic = new ProgramLogic(dao, new DomainMapper());
+				
+		ProgramEntity program = new ProgramEntity(UUID.randomUUID(), "Handstand One");
+		SequenceActionEntity programAction = new SequenceActionEntity(UUID.randomUUID());
+		SequenceActionEntity warmupAction = new SequenceActionEntity(UUID.randomUUID());
+		SequenceActionEntity skill = new SequenceActionEntity(UUID.randomUUID());
+		SupersetActionEntity strength = new SupersetActionEntity(UUID.randomUUID());
+		
+		program.setAction(programAction);
+		programAction.getSequence().addAll(Arrays.asList(warmupAction, skill, strength));
+		
+		dao.put(program);
+		
+		List<Program> readPrograms = logic.getAll();
+		DtoAssertions.assertThat(readPrograms).hasSize(1);
+		
+		
+		Program readProgram = readPrograms.get(0);
 		DtoAssertions.assertThat(readProgram).isNotNull().hasId(program.getUuid()).hasActionId(programAction.getUuid());
 		DtoAssertions.assertThat(readProgram.getAction()).hasId(programAction.getUuid()).isSequenceAction().containsExactlyActionIds(warmupAction.getUuid(), skill.getUuid(), strength.getUuid());
 	}
