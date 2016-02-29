479205b528790184673b2b83a1eb201e113e79ad
Harald Bittermann <harald.bittermann@gmail.com>
2016-02-29 21:39:44 +0100

    organize imports

--- ui/src/test/java/net/d80harri/coach/ui/testutils/JfxRule.java
+++ ui/src/test/java/net/d80harri/coach/ui/testutils/JfxRule.java
@@ -5,7 +5,6 @@ import org.junit.runner.Description;
 import org.junit.runners.model.Statement;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.stage.Stage;
 
 public class JfxRule implements TestRule {
